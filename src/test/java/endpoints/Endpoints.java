a4fb1b659024de37378c000078581a8aa83bab35
Calandarov <calandarov@icloud.com>
2024-04-04 09:55:00 +0300

    добавлен эндпоинт

--- src/test/java/endpoints/Endpoints.java
+++ src/test/java/endpoints/Endpoints.java
@@ -3,6 +3,7 @@ package endpoints;
 public class Endpoints {
     public static final String BASE_URI = "https://reqres.in";
     public static final String USERS = "/api/users";
+    public static final String UNKNOWN_RESOURCE = "/api/unknown";
     public static final String REGISTER = "/api/register";
     public static final String LOGIN = "/api/login";
 }
